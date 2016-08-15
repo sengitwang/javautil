b365260028acd7ba0bc5b773920939b2e70ecf3c
lenovo <lenovo>
2016-08-15 10:09:44 +0800

    saved

---
+++ src/main/java/com/senit/javautil/model/FileType.java
@@ -0,0 +1,7 @@
+package com.senit.javautil.model;
+
+/**
+ * Created by sen on 2016/8/15.
+ */
+public class FileType {
+}
---
+++ src/main/java/com/senit/javautil/util/EncryptUtil.java
@@ -0,0 +1,7 @@
+package com.senit.javautil.util;
+
+/**
+ * Created by sen on 2016/8/15.
+ */
+public class EncryptUtil {
+}
